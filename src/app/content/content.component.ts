d50f62f8f9b56e22ca051f5d77d7bd514f5d61db
jpasol <38642594+jpasol@users.noreply.github.com>
2021-01-28 18:22:28 +0800

    separated icon classes

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ContentComponent implements OnInit {
 
   constructor() { }
+  isEditMode: boolean = true;
 
   ngOnInit(): void {
   }
