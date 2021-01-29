b20922349db0b54f34f0577a05f9329538732921
jpasol <38642594+jpasol@users.noreply.github.com>
2021-01-29 16:14:59 +0800

    turned content to be editable, added functions to handle

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-content',
@@ -7,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentComponent implements OnInit {
 
+  @ViewChild('title') title: ElementRef
+  @ViewChild('content') content: ElementRef
   constructor() { }
-  isEditMode: boolean = true;
+  isEditMode: boolean;
 
+  
+  public set editMode(v : boolean) {
+    this.isEditMode = v;
+    this.title.nativeElement.disabled = !v
+    this.content.nativeElement.contenteditable = v
+  }
+  
+  
   ngOnInit(): void {
+  }
+
+  private toggleEditableText(){
+    this.editMode = !this.isEditMode
   }
 
 }
