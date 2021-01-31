9892f31f602c57a3815310add8cffa69313048a7
jpasol <38642594+jpasol@users.noreply.github.com>
2021-01-31 15:45:41 +0800

    formalized naming of speech data

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -7,7 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  shit: number[] = [1,2,3];
+  speeches: any[] = [];
   selected: any;
   constructor() { }
   @Input() choice: any;
